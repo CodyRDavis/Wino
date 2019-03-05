1cc631cec9c5a17cf2a414ce65644eb5d1927e67
Cody Davis <Cody.1337@gmail.com>
2019-03-05 15:27:39 -0600

    removed unnecessary console log from token reader.

--- api/v1/services/tokenReader.js
+++ api/v1/services/tokenReader.js
@@ -2,8 +2,7 @@
 module.exports.checkToken = (req, res, next) => {
 
     const authHeader = req.headers['authorization'];
-    console.log (req.headers);
-    console.log("Checking token",authHeader);
+
     if(typeof authHeader !== 'undefined') {
         return authHeader;
     }
