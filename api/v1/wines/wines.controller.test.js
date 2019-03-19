305b597683e844ff98506ccdf2fb9cf435c6f20f
Samuel Teich <steich@gmail.com>
2019-03-19 13:51:43 -0500

    working on converting tests in wines.controller.test.js from jest to mocha

--- api/v1/wines/wines.controller.test.js
+++ api/v1/wines/wines.controller.test.js
@@ -1,45 +1,45 @@
 const controller = require('./wines.controller')
 
-test('Testing Wine Controller - Update Wine Result', () => {
-  const req = {'body': {'email': 'thisemail@domain.com'}};
-  mockResultJson = jest.fn();
-  const res = {'status': ()=>{return {'json': mockResultJson};}};
-  mockPassedCallback = jest.fn();
-
-  controller.updateWine(req, res, mockPassedCallback);
-
-  expect(mockResultJson.mock.calls[0][0].success).toEqual(true);
-});
-
-test('Testing Wine Controller - Get Wine Result', () => {
-  const req = {'body': {'email': 'thisemail@domain.com'}};
-  mockResultJson = jest.fn();
-  const res = {'status': ()=>{return {'json': mockResultJson};}};
-  mockPassedCallback = jest.fn();
-
-  controller.getWine(req, res, mockPassedCallback);
-
-  expect(mockResultJson.mock.calls[0][0].success).toEqual(true);
-});
-
-test('Testing Wine Controller - Create Wine Result', () => {
-  const req = {'body': {'email': 'thisemail@domain.com'}};
-  mockResultJson = jest.fn();
-  const res = {'status': ()=>{return {'json': mockResultJson};}};
-  mockPassedCallback = jest.fn();
-
-  controller.createWine(req, res, mockPassedCallback);
-
-  expect(mockResultJson.mock.calls[0][0].success).toEqual(true);
-});
-
-test('Testing Wine Controller - Delete Wine Result', () => {
-  const req = {'body': {'email': 'thisemail@domain.com'}};
-  mockResultJson = jest.fn();
-  const res = {'status': ()=>{return {'json': mockResultJson};}};
-  mockPassedCallback = jest.fn();
-
-  controller.deleteWine(req, res, mockPassedCallback);
-
-  expect(mockResultJson.mock.calls[0][0].success).toEqual(true);
-});
+// test('Testing Wine Controller - Update Wine Result', () => {
+//   const req = {'body': {'email': 'thisemail@domain.com'}};
+//   mockResultJson = jest.fn();
+//   const res = {'status': ()=>{return {'json': mockResultJson};}};
+//   mockPassedCallback = jest.fn();
+//
+//   controller.updateWine(req, res, mockPassedCallback);
+//
+//   expect(mockResultJson.mock.calls[0][0].success).toEqual(true);
+// });
+//
+// test('Testing Wine Controller - Get Wine Result', () => {
+//   const req = {'body': {'email': 'thisemail@domain.com'}};
+//   mockResultJson = jest.fn();
+//   const res = {'status': ()=>{return {'json': mockResultJson};}};
+//   mockPassedCallback = jest.fn();
+//
+//   controller.getWine(req, res, mockPassedCallback);
+//
+//   expect(mockResultJson.mock.calls[0][0].success).toEqual(true);
+// });
+//
+// test('Testing Wine Controller - Create Wine Result', () => {
+//   const req = {'body': {'email': 'thisemail@domain.com'}};
+//   mockResultJson = jest.fn();
+//   const res = {'status': ()=>{return {'json': mockResultJson};}};
+//   mockPassedCallback = jest.fn();
+//
+//   controller.createWine(req, res, mockPassedCallback);
+//
+//   expect(mockResultJson.mock.calls[0][0].success).toEqual(true);
+// });
+//
+// test('Testing Wine Controller - Delete Wine Result', () => {
+//   const req = {'body': {'email': 'thisemail@domain.com'}};
+//   mockResultJson = jest.fn();
+//   const res = {'status': ()=>{return {'json': mockResultJson};}};
+//   mockPassedCallback = jest.fn();
+//
+//   controller.deleteWine(req, res, mockPassedCallback);
+//
+//   expect(mockResultJson.mock.calls[0][0].success).toEqual(true);
+// });
