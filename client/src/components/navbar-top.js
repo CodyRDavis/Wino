08070882028b6b47b53c2838220f7c2e6cbbf551
Cody Davis <Cody.1337@gmail.com>
2019-05-08 17:21:24 -0500

    blah -  cant get the navbar just right yet

--- client/src/components/navbar-top.js
+++ client/src/components/navbar-top.js
@@ -9,15 +9,14 @@ import './navbar-top.css'
 class Navbar extends Component {
     render() {
         return(
-            <div className = "navbar">
-                <div className="title"><h1>Wino</h1></div>
-                <div className="menu">
-                    <ul>
-                        <li>item 1</li>
-                        <li>item 2</li>
-                        <li>item 3</li>
-                    </ul>
-                </div>
+            <div className="navbar">
+                <h1>Wino</h1>
+                <ul className="topnav">
+                    <li><a className="active" href="#home">Home</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li className="right"><a href="#about">About</a></li>
+                </ul>
             </div>
         )
     }
